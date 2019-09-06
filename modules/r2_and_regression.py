8088cc99314ca0e989633c01eeb189d9aa4d1472
Sarah Gjermo <sarah@gjermo.com>
2019-09-06 16:10:26 +0200

    everything runs fine at jupyter, small adjustments

--- modules/r2_and_regression.py
+++ modules/r2_and_regression.py
@@ -16,12 +16,12 @@ from modules import write_and_show as ws
 
 #Global variables
 today = pd.to_datetime(time.strftime("%Y.%m.%d %H:%M"), format="%Y.%m.%d %H:%M", errors='ignore')  # now
-max_final_numb_kandidater = 25
-max_input_series = 196
+max_final_numb_kandidater = 20 #25
+max_input_series = 496 #196
 nb_weeks_tipping = 10  # number of weeks to do tipping back in time
 tz = pytz.timezone('Etc/GMT-1')
 columns = ['ant_kandidater', 'ant_serier', 'r2_modelled', 'r2_tippet', 'r2_samlet', 'short_period', 'max_p']
-first_period = 218  # Length of the long regression in weeks
+first_period = 219 #219  # Length of the long regression in weeks
 min_kandidater = 1
 
 
@@ -85,7 +85,7 @@ def run_regression(auto_input,
                     max_kandidater = 171
                     min_kandidater = 1
 
-                max_weeks = 288
+                max_weeks = 219 #288
                 min_weeks = 11
                 print('max ant. kandidater: {}, min ant. kandidater: {}'.format(max_kandidater, min_kandidater))
                 print('max ant. uker: {}, min ant. uker: {}'.format(max_weeks, min_weeks))
